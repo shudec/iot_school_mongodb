8a5e9152c05871e35a12c80dbd9eb68d7e3ada13
SHC <stephane.hudec@gmail.com>
2020-11-02 22:13:34 +0100

    Ex0 OK

--- Ex0.py
+++ Ex0.py
@@ -3,7 +3,7 @@ from DataLoader import DataLoader
 
 dataLoader = DataLoader('students.json')
 data = dataLoader.load()
-for d in data:
-    db.students.insert_many(data)
+db.students.remove()
+db.students.insert_many(data)
 
 print(db.students.find_one({"name": "Aurelia Menendez"}))
